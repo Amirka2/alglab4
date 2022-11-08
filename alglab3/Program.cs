e2cd64946a44f34699bd527ebc4eb401264969a5
Amir <70907990+Amirka2@users.noreply.github.com>
2022-11-08 18:14:16 +0500

    подготовлен под вставку методов сортировки

--- alglab3/Program.cs
+++ alglab3/Program.cs
@@ -8,26 +8,42 @@ namespace alglab3
         static void Main(string[] args)
         {
             Run2Task();
-            StreamWriter sw = new StreamWriter("123.txt", true);
-            sw.Write("123");
         }
         static void Run2Task()
         {
             Table table = DataWorker.GetTableFromFile("tables/countries.txt");
             Console.WriteLine("Выберите желаемый метод сортировки (1 - прямое, 2 - естественное, 3 - многопутевое.)");
-            //string sortOption = Console.ReadLine();
+            string sortOption = Console.ReadLine();
 
 
             Console.WriteLine($"Возможные атрибуты: {table.Header.GetAttributes()}");
             Console.WriteLine("Введите название ключевого атрибута: ");
-            //string attributeName = Console.ReadLine().Trim();
+            string attributeName = Console.ReadLine().Trim();
             Console.WriteLine("Введите значение ключевого атрибута: ");
-            //string attribute = Console.ReadLine().Trim();
+            string attribute = Console.ReadLine().Trim();
 
 
-            //Table filteredTable = DataWorker.GetFilteredTable(table, attributeName, attribute);
-            int[] arr = DataWorker.GetArrayFromTable(table);
-            int[] resultArr = { 10000000, 1500000000, 150000000 };
+            Table filteredTable = DataWorker.GetFilteredTable(table, attributeName, attribute);
+            int[] arr = DataWorker.GetArrayFromTable(filteredTable);
+            int[] resultArr = new int[arr.Length];
+
+            switch (sortOption)
+            {
+                case "1":
+                    //resultArr = sort1(arr);
+                    break;
+                case "2":
+                    //resultArr = sort2(arr);
+                    break;
+                case "3":
+                    //resultArr = sort3(arr);
+                    break;
+                default:
+                    Console.WriteLine("Введен некорректный способ сортировки");
+                    break;
+            }
+
+            
             DataWorker.RewriteTableBySort(table, resultArr, "tables/countriesSorted.txt"); 
 
         }
